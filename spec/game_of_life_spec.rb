fd3f099195ba2079ac6db0c181a160802e3386b7
JesusMava <jesusmacedo7@gmail.com>
2019-08-05 18:57:40 -0500

    I did proof for the principlas methods

--- spec/game_of_life_spec.rb
+++ spec/game_of_life_spec.rb
@@ -1,3 +1,4 @@
+=begin
 RSpec.describe GameOfLife do
   it "has a version number" do
     expect(GameOfLife::VERSION).not_to be nil
@@ -6,4 +7,20 @@ RSpec.describe GameOfLife do
   it "does something useful" do
     expect(false).to eq(true)
   end
+end
+=end
+RSpec.describe GameOfLife::Tableros do
+  tableros = GameOfLife::Tableros.new(2)
+  it "board's large " do
+    expect(tableros.large_boards).to eq(6)
+  end
+
+  it "board's width" do
+    expect(tableros.width_board).to eq(18)
+  end
+  
+  it "total squares" do
+    expect(tableros.total_squares).to eq(36)
+  end
+
 end
