bd9ca20b07c50ba2f5e05466e9b7f3ef09fa691f
JesusMava <jesusmacedo7@gmail.com>
2019-08-05 18:56:19 -0500

    started the proyect

---
+++ lib/game_of_life.rb
@@ -0,0 +1,6 @@
+require "game_of_life/version"
+
+module GameOfLife
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/game_of_life_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe GameOfLife do
+  it "has a version number" do
+    expect(GameOfLife::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
