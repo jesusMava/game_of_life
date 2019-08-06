94b47a50971b6af275efc58f4b54e74931081b3f
JesusMava <jesusmacedo7@gmail.com>
2019-08-06 09:52:07 -0500

    doing the divisor for take a mini tic tac toe

--- lib/game_of_life.rb
+++ lib/game_of_life.rb
@@ -5,40 +5,40 @@ module GameOfLife
   # calculate width,larga and total of squares to the board
   class Tableros
     attr_accessor :num_squares, :mini_colums, :width_square, :total_squares,
-    :original_board, :copycat_board
+                  :original_board, :copycat_board, :top_row, :medium_row, :low_row
     def initialize(num_squares)
       @num_squares = num_squares
     end
 
-    def large_boards
+    def method_large_boards
       @mini_colums = 3 * @num_squares
     end
 
-    def width_square
+    def method_width_square
       @width_square = 9 * @num_squares
     end
 
-    def total_squares
+    def method_total_squares
       @total_squares = 9 * @num_squares ** 2
     end
 
-    def create_origina_board
+    def method_create_origina_board
       @original_board = Array.new(@mini_colums) {Array.new(@mini_colums)}
     end
 
-    def copycat_board
+    def method_copycat_board
       @copycat_board =  @original_board
     end
 
-    def show_original_board
+    def method_show_original_board
       @original_board.each { |elem| puts elem.join(' | ') }
     end
 
-    def show_copycat_board
+    def method_show_copycat_board
       @copycat_board.each { |elem| puts elem.join(' | ') }
     end
 
-    def list_boards
+    def method_list_boards
       num = 0
       @copycat_board.map! do |elem_exter|
         elem_exter.map! do
@@ -47,90 +47,82 @@ module GameOfLife
       end
     end
 
-    def validate_input_user(input_user)
+    def method_validate_input_user(input_user)
       /[^\D]/.match(input_user)
     end
 
-    def last_line
+    def method_last_line
       @last_line = @total_squares - @mini_colums
     end
 
-    def position_row(input_user)
+    def method_position_row(input_user)
       @row = input_user / @mini_colums
     end
 
-    def position_columns(input_user)
+    def method_position_columns(input_user)
       @columna = input_user - (@row * @mini_colums)
     end
 
   end
-# start the game of life
+  # start the game of life
   class Play
     def initialize(copy_board)
       @copy_board = copy_board
     end
 
-    def top_row
+    def method_top_row
       columna = 0
-      top_row = []
+      @top_row = []
       loop do
-        top_row.push(@copycat_board[columna])
+        @top_row.push(@copycat_board[columna])
         columna += 3
         break if columna == @mini_colums
       end
-=begin
-      initial_position = 0
-      final_position = 2
-      top_row = 0
-      medium_row = 1
-      low_row = 2
-      loop do
-        (initial_position..final_position).each do |position|
-          puts position
-        end
-        initial_position += 3
-        final_position += 3
-        top_row += 1
-        medium_row += 1
-        low_row += 1
-        break if final_position > @total_squares - 1
-      end
-=end
     end
 
-    def medium_row
+    def method_medium_row
       columna = 1
-      medium_row = []
+      @medium_row = []
       loop do
-        medium_row.push(@copycat_board[columna])
-        columna += 3
-        break if columna > @mini_colums
-      end
-    end
-    def low_row
-      columna = 2
-      low_row = []
-      loop do
-        low_row.push(@copycat_board[columna])
+        @medium_row.push(@copycat_board[columna])
         columna += 3
         break if columna > @mini_colums
       end
     end
 
+    def method_low_row
+      columna = 2
+      @low_row = []
+      loop do
+        @low_row.push(@copycat_board[columna])
+        columna += 3
+        break if columna > @mini_colums
+      end
+    end
+
+    def comprobar
+      position = 0
+      until dividers < num_squares - 1
+        mini_check = []
+        mini_check.push(@top_row[position])
+        mini_check.push(@medium_row[position])
+        mini_check.push(@low_row[position])
+      end
+    end
   end
   tableros = Tableros.new(2)
   play = Play.new
-  tableros.large_boards
-  tableros.width_square
-  tableros.total_squares
-  tableros.create_origina_board
-  tableros.copycat_board
-  tableros.list_boards
+  tableros.method_large_boards
+  tableros.method_width_square
+  tableros.method_total_squares
+  tableros.method_create_origina_board
+  tableros.method_copycat_board
+  tableros.method_list_boards
   # tableros.show_copycat_board
-  tableros.last_line
-  play.top_row
-  play.medium_row
-  play.low_row
+  tableros.method_last_line
+  play.method_top_row
+  play.method_medium_row
+  play.method_low_row
   input_user = 0
   loop do
     puts 'Please, choose a position '
